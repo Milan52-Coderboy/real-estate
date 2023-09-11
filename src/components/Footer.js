c45dab719fcbfba0981f9372e2924be16ff90abc
Milan52-Coderboy <milandhola0502@gmail.com>
2023-09-11 15:23:54 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Img from '../assets/img/github.png';
 const Footer = () => {
   return (
     <>
-      <a href="https://github.com/Milan52-Coderboy/dreamhouse" target="_blank">
+      <a href="https://github.com/Milan52-Coderboy/real-estate" target="_blank" rel="noreferrer">
         <div class="fixed w-12 h-12 animate-bounce flex items-center justify-center bg-white rounded-full bottom-0 right-2">
           <img src={Img} alt="" />
         </div>
